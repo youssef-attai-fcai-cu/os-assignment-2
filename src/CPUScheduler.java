9cc513bb89355eb49aca380ea3a03907f1ab8284
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-02 17:18:44 +0200

    Add method to populate processes queue

--- src/CPUScheduler.java
+++ src/CPUScheduler.java
@@ -4,5 +4,9 @@ import java.util.Queue;
 public abstract class CPUScheduler {
     protected final Queue<Process> processes = new LinkedList<>();
 
+    public void addProcess(Process process) {
+        this.processes.add(process);
+    }
+
     public abstract void start();
 }
